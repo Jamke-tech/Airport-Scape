48e5ebcae728f0db7e293ba56a84e6a35ce02df4
miguel08171099 <71837104+miguel08171099@users.noreply.github.com>
2020-12-17 13:42:10 +0100

    Nuevo formato Mochilas/Objetos y Game

--- DAO/src/main/java/edu/upc/eetac/dsa/model/Game.java
+++ DAO/src/main/java/edu/upc/eetac/dsa/model/Game.java
@@ -2,42 +2,86 @@ package edu.upc.eetac.dsa.model;
 
 public class Game {
     public int id;
-    public MainCharacter character;
-    public Map map;
+    public String name;
+    public int idMap;
     public int time;
     public boolean win;
     public int idUser;
+    public int suspicious;
+    public boolean ticket;
 
-    public Game(int id, MainCharacter character, Map map, int time, boolean win, int idUser) {
+    public Game(int id, String name, int idMap, int time, boolean win, int idUser, int suspicious, boolean ticket) {
         this.id = id;
-        this.character = character;
-        this.map = map;
+        this.name = name;
+        this.idMap = idMap;
         this.time = time;
         this.win = win;
         this.idUser = idUser;
+        this.suspicious = suspicious;
+        this.ticket = ticket;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public void setCharacter(MainCharacter character) {
-        this.character = character;
+    public String getName() {
+        return name;
     }
 
-    public void setMap(Map map) {
-        this.map = map;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getIdMap() {
+        return idMap;
+    }
+
+    public void setIdMap(int idMap) {
+        this.idMap = idMap;
+    }
+
+    public int getTime() {
+        return time;
     }
 
     public void setTime(int time) {
         this.time = time;
     }
 
+    public boolean isWin() {
+        return win;
+    }
+
     public void setWin(boolean win) {
         this.win = win;
     }
 
+    public int getIdUser() {
+        return idUser;
+    }
+
     public void setIdUser(int idUser) {
         this.idUser = idUser;
+    }
+
+    public int getSuspicious() {
+        return suspicious;
+    }
+
+    public void setSuspicious(int suspicious) {
+        this.suspicious = suspicious;
+    }
+
+    public boolean isTicket() {
+        return ticket;
+    }
+
+    public void setTicket(boolean ticket) {
+        this.ticket = ticket;
     }
 }
