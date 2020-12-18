f58ff2d9a848bbf45122f0066fe2d7db563a2d16
gerardbelvis <gerard.belvis@gmail.com>
2020-12-18 10:22:39 +0100

    Creando mas pantallas

--- Log_IN_java/app/src/main/java/com/example/log_in_java/MainActivity.java
+++ Log_IN_java/app/src/main/java/com/example/log_in_java/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.log_in_java;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.log_in_java.models.User;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -11,6 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void profileButtonClicked(View v){
+        startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+    }
+
 }
 
 
