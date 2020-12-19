42f17b813a207c491662f1d4b18ab3dc06ae8eaf
AbelOrPa <abel.ortiz@estudiantat.upc.edu>
2020-12-19 20:33:33 +0100

    Beta 1.1 shop

--- DAO/public/JSON/shop.js
+++ DAO/public/JSON/shop.js
@@ -3,37 +3,41 @@ $(document).ready(function(){
 });
 
 $(function(){
-    $('#buttonBuy1').on('click',function(e){
-    console.log("Buy1 clicked"); //per fer proves
-            //Cancel the default action (navigation) of the click.
-            e.preventDefault();
+    var i = 1;
+    for (i = 1; i =6 ; i++){
+        $('#buttonBuy'+ i).on('click',function(e){
+        console.log("Buy1 clicked"); //per fer proves
+                //Cancel the default action (navigation) of the click.
+                e.preventDefault();
 
-    var username = document.getElementById("userName")
+        var username = document.getElementById("userName")
 
-       $.ajax({
-               type: "PUT",
-               url: "/gameDSA/object/buy/{userName}/1", //A definir cuando se haga bien el servicio
-               data: {username.value : "userName"}
-               success: function(data) {
-                   console.log("success");
-                   console.log(data.status)
-               },
-               complete: function(data) {
-                   if(data.status == 406){
-                      alert("You don't have enough money!");
+           $.ajax({
+                   type: "PUT",
+                   url: "/gameDSA/object/buy/{userName}/{i}", //A definir cuando se haga bien el servicio
+                   data: {username.value : "userName", i:"i"}
+                   success: function(data) {
+                       console.log("success");
+                       console.log(data.status)
+                   },
+                   complete: function(data) {
+                       if(data.status == 406){
+                          alert("You don't have enough money!");
+                       }
+                       else if (data.status != 200){
+                           alert("Good purchase!");
+                       }
+                       else if (data.status != 503){
+                           alert("Database down!");
+                       }
                    }
-                   else if (data.status != 200){
-                       alert("Good purchase!");
-                   }
-                   else if (data.status != 503){
-                       alert("Database down!");
-                   }
-               }
-       })
-    }
+           })
+        }
+
     })
+}
 })
-
+/*
 $(document).ready(function(){
     console.log("ok loaded")
 });
@@ -69,3 +73,14 @@ $(function(){
     }
     })
 })
+*/
+$(document).ready(function(){
+    console.log("ok loaded")
+});
+
+$(function(){
+    $('#shop.html').on('load',function(e){
+
+
+    });
+
