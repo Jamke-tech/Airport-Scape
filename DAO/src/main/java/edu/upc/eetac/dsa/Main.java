b5f4147538d88bdc4f24ec1544392532d1af4e15
miguel08171099 <71837104+miguel08171099@users.noreply.github.com>
2020-12-27 10:31:12 +0100

    Correccion maven para entorno produccion

--- DAO/src/main/java/edu/upc/eetac/dsa/Main.java
+++ DAO/src/main/java/edu/upc/eetac/dsa/Main.java
@@ -11,8 +11,8 @@ import java.net.URI;
 
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/gameDSA/";//aqui s'haura de modificar per posar en produccio
-
+    public static final String BASE_URI = "http://147.83.7.203/gameDSA/";//entorno produccion
+    //public static final String BASE_URI = "http://localhost:8080/gameDSA/";//entorno local
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
