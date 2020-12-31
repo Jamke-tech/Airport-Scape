918a981348c02d9c0186e8dec97ffedb593451aa
miguel08171099 <71837104+miguel08171099@users.noreply.github.com>
2020-12-31 15:03:07 +0100

    Entorno produccion otra vez

--- DAO/src/main/java/edu/upc/eetac/dsa/Main.java
+++ DAO/src/main/java/edu/upc/eetac/dsa/Main.java
@@ -11,7 +11,7 @@ import java.net.URI;
 
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://147.83.7.203:8080/gameDSA/";//entorno produccion
+    public static final String BASE_URI = "http://eetacdsa0.upc.es:8080/gameDSA/";//entorno produccion
     //public static final String BASE_URI = "http://localhost:8080/gameDSA/";//entorno local
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
@@ -27,7 +27,7 @@ public class Main {
 
         BeanConfig beanConfig = new BeanConfig();
 
-        beanConfig.setHost("147.83.7.203:8080");//entorno produccion
+        beanConfig.setHost("eetacdsa0.upc.es:8080");//entorno produccion
         //beanConfig.setHost("localhost:8080");//entorno local
         beanConfig.setBasePath("/gameDSA");
         beanConfig.setContact("support@example.com");
