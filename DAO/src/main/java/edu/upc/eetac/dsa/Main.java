b23a6e4c74d1246bf1497e0a1f7081164ec2856b
miguel08171099 <71837104+miguel08171099@users.noreply.github.com>
2021-01-02 14:12:16 +0100

    Produccion (seguimos)

--- DAO/src/main/java/edu/upc/eetac/dsa/Main.java
+++ DAO/src/main/java/edu/upc/eetac/dsa/Main.java
@@ -1,7 +1,6 @@
 package edu.upc.eetac.dsa;
 
 import io.swagger.jaxrs.config.BeanConfig;
-import io.swagger.jersey.listing.ApiListingResourceJSON;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.grizzly.http.server.StaticHttpHandler;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
@@ -10,14 +9,10 @@ import org.glassfish.jersey.server.ResourceConfig;
 import java.io.IOException;
 import java.net.URI;
 
-/**
- * Main class.
- *
- */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://eetacdsa0.upc.es:8080/gameDSA/";
-
+    public static final String BASE_URI = "http://eetacdsa0.upc.es:8080/gameDSA/";//entorno produccion
+    //public static final String BASE_URI = "http://localhost:8080/gameDSA/";//entorno local
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
      * @return Grizzly HTTP server.
@@ -25,35 +20,29 @@ public class Main {
     public static HttpServer startServer() {
         // create a resource config that scans for JAX-RS resources and providers
         // in edu.upc.dsa package
-        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.eetac.dsa.services");
+        final ResourceConfig rc = new ResourceConfig().packages("edu.upc.eetac.dsa.service");
 
         rc.register(io.swagger.jaxrs.listing.ApiListingResource.class);
         rc.register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 
         BeanConfig beanConfig = new BeanConfig();
 
-        beanConfig.setHost("eetacdsa0.upc.es:8080");
+
+        beanConfig.setHost("eetacdsa0.upc.es:8080");//entorno produccion
+        //beanConfig.setHost("localhost:8080");//entorno local
         beanConfig.setBasePath("/gameDSA");
         beanConfig.setContact("support@example.com");
-        beanConfig.setDescription("REST API for Tracks Manager");
+        beanConfig.setDescription("REST API for GAMESERVICE DSA");
         beanConfig.setLicenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html");
-        beanConfig.setResourcePackage("edu.upc.dsa.services");
+        beanConfig.setResourcePackage("edu.upc.eetac.dsa.service");
         beanConfig.setTermsOfServiceUrl("http://www.example.com/resources/eula");
         beanConfig.setTitle("REST API");
         beanConfig.setVersion("1.0.0");
         beanConfig.setScan(true);
 
-        // create and start a new instance of grizzly http server
-        // exposing the Jersey application at BASE_URI
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
-
-    /**
-     * Main method.
-     * @param args
-     * @throws IOException
-     */
     public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
 
@@ -61,11 +50,16 @@ public class Main {
         server.getServerConfiguration().addHttpHandler(staticHttpHandler, "/");
 
 
-        System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        /*System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));*/
 
+        String swagger_uri = BASE_URI;
+        String target = "gameDSA";
+        String replacement = "swagger";
+        swagger_uri = swagger_uri.replace(target, replacement);
+        System.out.println(String.format("RestApi Started at " + "%s\nHit enter to stop it...", swagger_uri));
         System.in.read();
+        //server.shutdownNow();
         server.stop();
     }
 }
-
