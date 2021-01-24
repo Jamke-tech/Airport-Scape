fa38e9e6828f3490231dda111504958355ece7a2
AbelOrPa <abel.ortiz@estudiantat.upc.edu>
2021-01-24 09:44:57 +0100

    Botones web + perfil

--- DAO/public/JSON/login.js
+++ DAO/public/JSON/login.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     console.log("ok loaded")
+    myStorage = window.localStorage;
 });
 //prova commit
 $(function(){
@@ -42,15 +43,16 @@ $(function(){
                     console.log("success");
                     console.log(data.status);
 
-                    sessionStorage.setItem(data.id, data.userName);
-                    var  userNameStored = sessionStorage.getItem(data.id);
+                    myStorage.setItem("user",data.username);
+                    var  userNameStored = sessionStorage.getItem("user");
                     document.getElementById("navbarDropdown").innerHTML = data.userName;
-                    window.location.href = 'inicio.html' + '#' + data.id;
+                    window.location.pathname = "/swagger/inicio.html"
 
                 },
                 complete: function(data) {
                     if(data.status == 401){
                         alert("Wrong password or username");
+
                     }
                     else if (data.status == 404){
                         alert("User not registered");
@@ -65,62 +67,3 @@ $(function(){
     })
 })
 
-$(document).ready(function(){
-    $(function(){
-        $('#buttonInicio').on('click',function(e){
-            console.log("Go to Inicio"); //per fer proves
-                    //Cancel the default action (navigation) of the click.
-                    e.preventDefault();
-            var id = window.location.hash.substring(1);
-            var username = sessionStorage.getItem(id);
-            window.location.href = 'inicio.html' + '#' + id;
-            return false;
-        })
-    });
-});
-
-$(document).ready(function(){
-    $(function(){
-        $('#buttonLogIn').on('click',function(e){
-            console.log("Go to LogIn");
-
-            e.preventDefault();
-            var id = window.location.hash.substring(1);
-            if (id = null){
-                id = 0;
-            }
-            var username = sessionStorage.getItem(id);
-            window.location.href = 'index.html' + '#' + id;
-            return false;
-
-        })
-    });
-});
-
-$(document).ready(function(){
-    $(function(){
-        $('#buttonStatics').on('click',function(e){
-            console.log("Go to statistics");
-                    e.preventDefault();
-            var id = window.location.hash.substring(1);
-            var username = sessionStorage.getItem(id);
-            window.location.href = 'estadisticas.html' + '#' + id;
-            return false;
-        })
-    });
-});
-
-$(document).ready(function(){
-    $(function(){
-        $('#buttonShop').on('click',function(e){
-            console.log("Go to shop"); //per fer proves
-                    //Cancel the default action (navigation) of the click.
-                    e.preventDefault();
-            var id = window.location.hash.substring(1);
-            var username = sessionStorage.getItem(id);
-            window.location.href = 'shop.html' + '#' + id;
-            return false;
-
-        })
-    });
-});
