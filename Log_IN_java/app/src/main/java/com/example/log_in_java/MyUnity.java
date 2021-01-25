aa0866b0c9701b6c1bb38271258576a110a80578
Jamke-tech <jaume_taber10@hotmail.com>
2021-01-25 22:25:26 +0100

    My Unity

--- Log_IN_java/app/src/main/java/com/example/log_in_java/MyUnity.java
+++ Log_IN_java/app/src/main/java/com/example/log_in_java/MyUnity.java
@@ -1,18 +1,9 @@
 package com.example.log_in_java;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
-import com.example.log_in_java.models.Game;
 import com.example.log_in_java.models.Map;
-import com.example.log_in_java.models.User;
 import com.example.log_in_java.services.GameManagerService;
 import com.unity3d.player.UnityPlayerActivity;
 
@@ -24,78 +15,12 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class PlayGameActivity extends AppCompatActivity {
+public class MyUnity extends UnityPlayerActivity {
 
-    private SharedPreferences preferences;
-    private String nickname;
-    private int level;
-    private String suspicious;
-    private String money;
     private static Retrofit retrofit;
     private GameManagerService gameAPI;
     String stringMap;
 
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_game);
-        loadPreferences();
-        nickname =preferences.getString("userNickname", null);//recupero el nickname
-
-        Bundle extras = getIntent().getExtras();
-        if (extras != null){
-            suspicious = extras.getString("suspicious");
-            money = extras.getString("money");
-        }
-        startRetrofit();
-
-
-    }
-
-
-    public void newYorkClick (View v){
-
-        level = 4;
-        //GetMapa(level);
-        Intent newintent = new Intent(this, UnityPlayerActivity.class);
-        newintent.putExtra("playerSuspicious",suspicious);
-        newintent.putExtra("playerMoney",money);
-        newintent.putExtra("playerNickname",nickname);
-        newintent.putExtra("playerLevel",level);
-        startActivity(newintent);
-
-    }
-
-    public void frankfurtClick (View v){
-
-        level=1;
-        //GetMapa(level);
-
-        Intent newintent = new Intent(this, UnityPlayerActivity.class);
-        newintent.putExtra("playerSuspicious",suspicious);
-        newintent.putExtra("playerMoney",money);
-        newintent.putExtra("playerNickname",nickname);
-        newintent.putExtra("playerLevel",level);
-        startActivity(newintent);
-
-
-    }
-
-    public void moscowClick (View v){
-
-        level=7;
-        //GetMapa(level);
-        Intent newintent = new Intent(this, UnityPlayerActivity.class);
-        newintent.putExtra("playerSuspicious",suspicious);
-        newintent.putExtra("playerMoney",money);
-        newintent.putExtra("playerNickname",nickname);
-        newintent.putExtra("playerLevel",level);
-        startActivity(newintent);
-
-
-    }
     private static void startRetrofit(){
         HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
         interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
@@ -108,11 +33,9 @@ public class PlayGameActivity extends AppCompatActivity {
                 .client(client)
                 .build();
     }
-    private void loadPreferences(){
-        preferences = getSharedPreferences("Login credentials", Context.MODE_PRIVATE);
-    }
 
-    public String GetMapa(int level){
+    public String GetMap(int level){
+        startRetrofit();
 
         gameAPI = retrofit.create(GameManagerService.class);
         Call<Map> call = gameAPI.getStringMap(level);
