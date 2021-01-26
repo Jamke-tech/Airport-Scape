eca95fa1b3f69f57b1be2e905b46e27763437513
jcardenasmarquez <juan.antonio.cardenas@estudiantat.upc.edu>
2021-01-26 20:03:38 +0100

    Cambio en stats.js

--- DAO/public/JSON/stats.js
+++ DAO/public/JSON/stats.js
@@ -2,10 +2,23 @@ $(document).ready(function(){
 
     var staticUrl = '/gameDSA/user/ranking';
 
+
     $.getJSON(staticUrl, function(data){
         $(data).each(function(index,value){
             var user = value.userName;
-            var concat ='<tbody><tr><th scope="row">'+(index+1)+"</th><td>"+user+"</td><td>"+value.money+"</td><td>"+"..."+"</td></tr></tbody>";
+            var money = value.money;
+            var gamesWon = 0;
+            var staticUrl2 = "/gameDSA/game/getList/"+user;
+            $.getJSON(staticUrl2, function(data){
+                $(data).each(function(index,value){
+                    var win = value.win;
+                    if(win==true){
+                        gamesWon = gamesWon+1;
+                    }
+                });
+            })
+
+            var concat ='<tbody><tr><th scope="row">'+(index+1)+"</th><td>"+user+"</td><td>"+money+"</td><td>"+gamesWon+"</td></tr></tbody>";
             $("table").append(concat);
 
            /*var graphs = [
